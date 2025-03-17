95e0bbd94ae0d3b333a3090a086a5a07a8e62bcc
rashmi0710 <dongrerashmi15@gmail.com>
2025-03-17 22:23:49 +0530

    debugging completedgit add .

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,49 @@
-import { useEffect, useState } from 'react'
-import { TodoProvider } from './assets/contexts/TodoContext';
-import TodoForm from './components/TodoForm';
-import TodoItem from './components/TodoItem';
-
+import { useState, useEffect } from 'react'
+import {TodoProvider} from './contexts'
 import './App.css'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, setTodos] = useState([])
-  const addTodo = (todo)=>{
-    setTodos((prev)=> [{id:Date.now(), ...todo},...prev])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
   }
-  const updateTodo = (id, updatedTodo) => {
-    setTodos((prev) =>
-      prev.map((prevTodo) => (prevTodo.id === id ? { ...prevTodo, ...updatedTodo } : prevTodo))
-    );
-  };
-  
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
+
+    
+  }
+
   const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((todo)=> todo.id !== id))
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
   }
 
-  const toggleComplete = (id)=>{
-    setTodos((prev)=> prev.map((prevTodo)=>prevTodo.id===id ? {...prevTodo, completed: !prevTodo.completed}:prevTodo))
+  const toggleComplete = (id) => {
+    //console.log(id);
+    setTodos((prev) => 
+    prev.map((prevTodo) => 
+      prevTodo.id === id ? { ...prevTodo, 
+        completed: !prevTodo.completed } : prevTodo))
   }
+
   useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
-    setTodos(storedTodos);
-  }, []);
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
   
 
-  useEffect(()=> {
-    localStorage.setItem("todos",JSON.stringify(todos))
-  },[todos])
 
-  
+
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
