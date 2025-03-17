c45f240017ac3ea9c164b57b2175005ec80e6d12
rashmi0710 <dongrerashmi15@gmail.com>
2025-03-17 13:13:54 +0530

    added functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { TodoProvider } from './assets/contexts'
 import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([])
 
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-
-    </>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                        <TodoForm />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
   )
 }
 
